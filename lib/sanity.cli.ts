9d2b74ae01101fc24c88b483d77785c4749873c8
yelpcamp-profile <kolayem44@gmail.com>
2023-05-26 23:49:34 +0100

    first commit

--- lib/sanity.cli.ts
+++ lib/sanity.cli.ts
@@ -1,7 +1,8 @@
-import defineCliConfig from '@sanity/client'
+import  { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
+    api: {
     projectId: 'z6vp071g',
     dataset: 'production'
-  
+    }
 })
